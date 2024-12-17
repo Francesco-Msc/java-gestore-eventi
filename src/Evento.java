e91916efbfb5ca845fe8e7394834fcee92b9e425
Francesco-Msc <moscafrancesco1998@gmail.com>
2024-12-17 17:16:32 +0100

    added constructor for Event class with date and seat count validation

--- src/Evento.java
+++ src/Evento.java
@@ -5,5 +5,17 @@ public class Evento {
     private LocalDate date;
     private int totalSeats;
     private int bookedSeats;
-    
+
+    public Evento(String title, LocalDate date, int totalSeats){
+
+        if (date.isBefore(LocalDate.now())) {
+            throw new IllegalArgumentException("La data dell'evento è gia passata");
+        } else if (totalSeats <= 0) {
+            throw new IllegalArgumentException("Il numero dei posti deve essere positivo");            
+        }
+        this.title = title;
+        this.date = date;
+        this.totalSeats = totalSeats;
+        this.bookedSeats = 0;
+    }
 }
