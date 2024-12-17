7680c159f385adb651d0e131f41e7f8f13eb6307
Francesco-Msc <moscafrancesco1998@gmail.com>
2024-12-17 17:43:13 +0100

    added loop to add and cancel reservations

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
+        boolean add = true;
+        boolean remove = true;
+
         //Input informazioni evento
         Scanner input = new Scanner(System.in);
         System.out.println("Inserisci il titlo dell'evento");
@@ -31,5 +33,40 @@ public class Main {
         LocalTime time = LocalTime.parse(timeString, timeFormatter);
 
         Concerto event = new Concerto(title, date, totalSeats, time, price);
+
+        //Ciclo per aggiungere prenotazioni
+        while (add) {
+            System.out.println("Desideri aggiungere delle prenotazioni? S/N");
+            String answer = input.nextLine();
+            if (answer.equalsIgnoreCase("S")) {
+                System.out.println("Inserisci il numero di posti che desideri prenotare. N.B. Sono disponibili " + (totalSeats - event.getReservedSeats()) + " posti");
+                int reserve = input.nextInt();
+                input.nextLine();
+
+                event.makeReservation(reserve);
+                System.out.println("Hai prenotato " + reserve + " posti");
+            } else if (!answer.equalsIgnoreCase("S")) {
+                add = false;
+            }
+        }
+
+        //Ciclo per rimuovere prenotazioni
+        if (event.getReservedSeats() > 0) {
+            while (remove) {
+                System.out.println("Desideri rimuovere delle prenotazioni? S/N");
+                String answer = input.nextLine();
+                if (answer.equalsIgnoreCase("S")) {
+                    System.out.println("Inserisci il numero di prenotazioni che desideri rimuovere. N.B. Ci sono " + event.getReservedSeats() + " prenotazioni");
+                    int reserve = input.nextInt();
+                    input.nextLine();
+    
+                    event.cancelReservation(reserve);
+                    System.out.println("Hai disdetto " + reserve + " prenotazioni");
+                } else if (!answer.equalsIgnoreCase("S")) {
+                    remove = false;
+                }
+            }
+        }
+        input.close();
     }
 }
