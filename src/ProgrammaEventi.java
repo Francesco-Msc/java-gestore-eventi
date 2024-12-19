54cbe5f40f686652efb4c03ae6a7eb76774271fd
Francesco-Msc <moscafrancesco1998@gmail.com>
2024-12-19 14:59:20 +0100

    added methods to add events, clear all events on list and get number of events on list

--- src/ProgrammaEventi.java
+++ src/ProgrammaEventi.java
@@ -9,4 +9,16 @@ public class ProgrammaEventi {
         this.title = title;
         this.events = new ArrayList<>();
     }
+
+    public void addEvent(Evento event){
+        events.add(event);
+    }
+
+    public void clearEvents(){
+        events.clear();
+    }
+
+    public int getEvents(){
+        return events.size();
+    }
 }
